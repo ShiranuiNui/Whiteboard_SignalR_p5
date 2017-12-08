07caefa848c3e21c9c93b4836c218fc32b3b9794
ShiranuiNui <shiranuisayaka@gmail.com>
2017-12-08 19:59:44 +0900

    [Fix] URI Fixed For Deploy

--- wwwroot/scripts/sketch.js
+++ wwwroot/scripts/sketch.js
@@ -1,28 +1,24 @@
 let connection;
-function setup()
-{
+function setup() {
     createCanvas(640, 480);
     frameRate(30);
     strokeWeight(3);
     background(255, 255, 255);
     let button = createButton('ぜんしょうきょ');
     button.mousePressed(allDelete)
-    connection = new signalR.HubConnection('/draw');
-    connection.on('draw', function (prev_x, prev_y, x, y)
-    {
+    let uri = document.location.host.substring(0, document.location.host.indexOf('sketch.js'));
+    connection = new signalR.HubConnection(`${uri}/draw`);
+    connection.on('draw', function (prev_x, prev_y, x, y) {
         drawLineonReceived(prev_x, prev_y, x, y)
     });
-    connection.on('init', function (data)
-    {
+    connection.on('init', function (data) {
         initialCoordinates = JSON.parse(data);
         console.log(initialCoordinates);
-        initialCoordinates.forEach(element =>
-        {
+        initialCoordinates.forEach(element => {
             drawLineonReceived(element["PreviousX"], element["PreviousY"], element["NewX"], element["NewY"]);
         });
     });
-    connection.on('alldelete', function (data)
-    {
+    connection.on('alldelete', function (data) {
         background(255, 255, 255);
         rect(0, 0, 639, 479);
 
@@ -33,33 +29,26 @@ function setup()
 let past_x = 0;
 let past_y = 0;
 
-function draw()
-{
-    if (mouseIsPressed)
-    {
+function draw() {
+    if (mouseIsPressed) {
         drawLineonSelf();
         past_x = mouseX;
         past_y = mouseY;
     }
 }
-function mousePressed()
-{
+function mousePressed() {
     past_x = mouseX;
     past_y = mouseY;
 }
-function drawLineonSelf() 
-{
-    if (past_x != mouseX || past_y != mouseY)
-    {
+function drawLineonSelf() {
+    if (past_x != mouseX || past_y != mouseY) {
         line(mouseX, mouseY, past_x, past_y);
         connection.invoke('draw', past_x, past_y, mouseX, mouseY);
     }
 }
-function drawLineonReceived(x1, y1, x2, y2)
-{
+function drawLineonReceived(x1, y1, x2, y2) {
     line(x1, y1, x2, y2);
 }
-function allDelete()
-{
+function allDelete() {
     connection.invoke('alldelete');
 }
